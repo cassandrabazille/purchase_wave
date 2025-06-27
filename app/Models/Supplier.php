af0fa70bc2f6d46a95b92ab46d61a9301679465e
Cassandra Bazille <bazillecassandra@gmail.com>
2025-06-27 20:21:42 +0200

    US2.3 Supplier

---
+++ app/Models/Supplier.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\User;
+
+class Supplier extends Model
+{
+
+    protected $fillable = ['name', 'email','telephone', 'address'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'supplier_id', 'id');
+    }
+
+     public function orders()
+    {
+        return $this->hasMany(Order::class, 'supplier_id', 'id');
+    }
+
+     public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
