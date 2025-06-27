a2487eb2cd8cb77639e00f762eb340cdcd119ba1
Cassandra Bazille <bazillecassandra@gmail.com>
2025-06-27 20:24:20 +0200

    US2.5 Order

---
+++ app/Models/Order.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Supplier;
+use App\Models\User;
+use App\Models\OrderItem;
+class Order extends Model
+{
+    protected $fillable = ['reference', 'order_date', 'expected_delivery_date', 'confirmed_delivery_date', 'status','order_amount'];
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id', 'id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id', 'id');
+    }
+}
