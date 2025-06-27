a2ab56e5ffa50872a4ab4cd7cde4aeb43efe83c6
Cassandra Bazille <bazillecassandra@gmail.com>
2025-06-27 20:23:23 +0200

    US2.4 Product

---
+++ app/Models/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use App\Models\Supplier;
+use App\Models\User;
+use App\Models\OrderItem;
+
+class Product extends Model
+{
+    protected $fillable = ['reference', 'slug', 'description', 'price', 'image'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'product_id', 'id');
+    }
+}
